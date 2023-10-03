f6eaea2bfd648af2c9ee7fb21725dfc9c1f42bd4
Giang <nguyenvietgiang1110@gmail.com>
2023-10-03 15:58:18 +0700

    Lên giao diện đăng nhập, đăng ký

---
+++ app/src/main/java/com/example/g05fitstore/Client/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.g05fitstore.Client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.g05fitstore.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
