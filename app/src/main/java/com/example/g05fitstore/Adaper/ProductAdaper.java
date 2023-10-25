1afe8c936de2a6f7c8e8b827315b4098772fc3ad
riggo2019 <riggo2019@gmail.com>
2023-10-25 17:11:24 +0700

    Quản lí sản phẩm

--- app/src/main/java/com/example/g05fitstore/Adaper/ProductAdaper.java
+++ app/src/main/java/com/example/g05fitstore/Adaper/ProductAdaper.java
@@ -1,63 +1,84 @@
 package com.example.g05fitstore.Adaper;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.g05fitstore.Models.Product;
 import com.example.g05fitstore.R;
 
-import java.util.ArrayList;
+import java.util.List;
+
 
 public class ProductAdaper extends RecyclerView.Adapter<ProductAdaper.ProductViewHolder>{
-    ArrayList<Product> productArrayList;
-    Context context;
 
-    public ProductAdaper(ArrayList<Product> productArrayList, Context context) {
-        this.productArrayList = productArrayList;
-        this.context = context;
+    private List<Product> mListProduct;
+    private IClickListener mIClickListener;
+    public interface IClickListener {
+        void onClickUpdateItem(Product product);
+        void onCLickDeleteItem(Product product);
     }
 
-    @NonNull
+    public ProductAdaper(List<Product> mListProduct, IClickListener listener) {
+        this.mListProduct = mListProduct;
+        this.mIClickListener = listener;
+    }
+
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.product_item, parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
         return new ProductViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
-        Glide.with(context).load(productArrayList.get(position).getImage()).into(holder.imageView);
-        holder.textName.setText(productArrayList.get(position).getName());
-        holder.textDis.setText(productArrayList.get(position).getDiscription());
+        Product product = mListProduct.get(position);
+        if(product ==  null) {
+            return;
+        }
+        holder.tvName.setText(product.getName());
+        holder.tvDesc.setText(product.getDesc());
+
+        holder.IbtnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mIClickListener.onClickUpdateItem(product);
+            }
+        });
+
+        holder.IbtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mIClickListener.onCLickDeleteItem(product);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        if(productArrayList == null){
-            return 0;
+        if (mListProduct != null) {
+            return mListProduct.size();
         }
-        else {
-            return productArrayList.size();
-        }
+        return 0;
     }
 
     public class ProductViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
-        TextView textName;
-        TextView textDis;
+
+        private TextView tvName;
+        private TextView tvDesc;
+        private ImageButton IbtnUpdate, IbtnDelete;
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.product_image);
-            textName = itemView.findViewById(R.id.product_name);
-            textDis = itemView.findViewById(R.id.product_discription);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvDesc = itemView.findViewById(R.id.tv_desc);
+            IbtnUpdate = (ImageButton) itemView.findViewById(R.id.ibtn_Update);
+            IbtnDelete = (ImageButton) itemView.findViewById(R.id.ibtn_Delete);
         }
     }
+
 }
