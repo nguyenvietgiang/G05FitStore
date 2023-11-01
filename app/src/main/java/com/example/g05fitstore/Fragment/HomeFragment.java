df238441aaee90aa5a8e2d7684b10404931f807c
ducloc <locdivine@gmail.com>
2023-11-01 20:28:09 +0700

    search

--- app/src/main/java/com/example/g05fitstore/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/g05fitstore/Fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.g05fitstore.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.g05fitstore.Adaper.HomeAdapter;
 import com.example.g05fitstore.Adaper.ProductAdaper;
@@ -31,17 +35,28 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     HomeAdapter homeAdapter;
     ArrayList<Product> productArrayList;
+    EditText edtSearchName;
+    ImageButton searchBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         initUi(view);
-        getListProduct();
+        getListProduct("");
+
+        searchBtn.setOnClickListener(v -> {
+            searchProduct();
+        });
+
+
         return view;
     }
 
     private void initUi(View view) {
+        edtSearchName = view.findViewById(R.id.search_username_input);
+        searchBtn = view.findViewById(R.id.search_user_btn);
+
         recyclerView = view.findViewById(R.id.recycler_view);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -54,19 +69,43 @@ public class HomeFragment extends Fragment {
         recyclerView.setAdapter(homeAdapter);
     }
 
+    private void searchProduct() {
+        String strKey = edtSearchName.getText().toString().trim();
+        if (productArrayList != null) {
+            productArrayList.clear();
+        } else {
+            productArrayList = new ArrayList<>();
+        }
+        getListProduct(strKey);
+//        GlobalFunction.hideSoftKeyboard(getActivity());
+    }
+
     // Hiện danh sách sản phẩm lên recycleview
-    private void getListProduct() {
+    private void getListProduct(String key) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference productRef = database.getReference().child("list_product");
 
         productRef.addChildEventListener(new ChildEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 Product product = snapshot.getValue(Product.class);
-                if (product != null) {
-                    productArrayList.add(product);
-                    homeAdapter.notifyDataSetChanged();
+//                if (product != null) {
+//                    productArrayList.add(product);
+//                    homeAdapter.notifyDataSetChanged();
+//                }
+                if (product == null || productArrayList == null || homeAdapter == null) {
+                    return;
                 }
+                if (key.isEmpty()) {
+                    productArrayList.add(0, product);
+                } else {
+                    if (product.getName().toLowerCase().trim()
+                            .contains(key.toLowerCase().trim())) {
+                        productArrayList.add(0, product);
+                    }
+                }
+                homeAdapter.notifyDataSetChanged();
             }
 
             @Override
